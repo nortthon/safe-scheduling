2627fb7e95a02d6a7806621d0994480e0ffdd2c4
Lucas Augusto <nortthon@gmail.com>
2019-06-28 20:28:21 -0300

    new redis provider

---
+++ redis-provider/src/main/java/io/github/nortthon/safe/scheduling/provider/redis/RedisProvider.java
@@ -0,0 +1,27 @@
+package io.github.nortthon.safe.scheduling.provider.redis;
+
+import io.github.nortthon.safe.scheduling.Provider;
+import io.github.nortthon.safe.scheduling.SchedulerConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class RedisProvider implements Provider {
+
+    private final StringRedisTemplate redisTemplate;
+
+    @Override
+    public void execute(final Runnable task, final SchedulerConfig schedulerConfig) {
+        final Duration duration = Duration.ofMillis(schedulerConfig.getLockedFor());
+        final String now = String.valueOf(new Date().toInstant().getEpochSecond());
+
+        Optional.ofNullable(redisTemplate.opsForValue()
+                .setIfAbsent(schedulerConfig.getName(), now, duration))
+                .filter(s -> s)
+                .ifPresent(s -> task.run());
+    }
+}
---
+++ redis-provider/src/test/java/io/github/nortthon/safe/scheduling/provider/redis/RedisProviderTest.java
@@ -0,0 +1,75 @@
+package io.github.nortthon.safe.scheduling.provider.redis;
+
+import io.github.nortthon.safe.scheduling.Provider;
+import io.github.nortthon.safe.scheduling.SchedulerConfig;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.*;
+
+@DataRedisTest
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@RunWith(SpringRunner.class)
+public class RedisProviderTest {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    private RedisServer redisServer;
+
+    private Provider provider;
+
+    @Before
+    public void setup() throws IOException {
+        this.provider = new RedisProvider(redisTemplate);
+        this.redisServer = new RedisServer(6379);
+        this.redisServer.start();
+    }
+
+    @Test
+    public void testLockingAndRunningTaskScheduled() {
+        final SchedulerConfig config = SchedulerConfig.builder().name("task1").lockedFor(1000L).build();
+
+        final AtomicBoolean runned = new AtomicBoolean(false);
+
+        provider.execute(() -> runned.set(true), config);
+
+        assertTrue(runned.get());
+        assertEquals(true, redisTemplate.hasKey("task1"));
+    }
+
+    @Test
+    public void testLockedTask() {
+        final SchedulerConfig config = SchedulerConfig.builder().name("task2").lockedFor(1000L).build();
+
+        final String now = String.valueOf(new Date().toInstant().getEpochSecond());
+        redisTemplate.opsForValue().set(config.getName(), now, Duration.ofMillis(2000L));
+
+        final AtomicBoolean runned = new AtomicBoolean(false);
+
+        provider.execute(() -> runned.set(true), config);
+
+        assertFalse(runned.get());
+        assertEquals(true, redisTemplate.hasKey("task2"));
+    }
+
+    @After
+    public void finish() {
+        this.redisServer.stop();
+    }
+}
